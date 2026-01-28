8fb2c45edf6a1bae07077d3f65411797f53538c2
Redum9 <dumaneckijdenis@gmail.com>
2026-01-28 15:51:43 +0200

    solve the task

--- app/main.py
+++ app/main.py
@@ -7,13 +7,10 @@ def get_human_age(cat_age: int, dog_age: int) -> list[int]:
         result[0] += 1
     if cat_age > 24:
         result[0] += (cat_age - 24) // 4
-        
-
     if dog_age >= 15:
         result[1] += 1
     if dog_age >= 24:
         result[1] += 1
     if dog_age > 24:
         result[1] += (dog_age - 24) // 5
-        
     return result
