b53de7c32e62e4276dc894b8dd1b411406666e8c
Redum9 <dumaneckijdenis@gmail.com>
2026-01-28 15:44:41 +0200

    resolve

--- app/test_main.py
+++ app/test_main.py
@@ -4,6 +4,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_age",
     [
+        (-1, -1, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
